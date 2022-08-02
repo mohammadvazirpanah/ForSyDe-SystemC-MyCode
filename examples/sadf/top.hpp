c5728577d240e01475b8383282fd2e721bd372ba
mohammad <mohammad.vazirpanah@yahoo.com>
2022-08-02 08:51:45 +0430

    using helper

--- examples/sadf/top.hpp
+++ examples/sadf/top.hpp
@@ -104,8 +104,42 @@ SC_MODULE(top)
  
     SC_CTOR(top)
     {       
-        auto constant1 = new SADF::constant<int>("constant1", 1, 0);
-        constant1->oport1(form_constant);
+
+        SADF::make_constant ("constant1", 1, 0, form_constant);
+
+        SADF::make_detector12 ("detector12"
+                        ,gamma_func_detector,
+                        next_state_func_detector,
+                        output_decode_func_detector,
+                        S1,
+                        from_detector1,
+                        from_detector2,
+                        form_constant);
+
+        SADF::make_kernel ("kernel1",
+                            func_kernel1,
+                            table,
+                            from_kernel1,
+                            to_kernel1,
+                            from_detector1
+                            );
+
+        SADF::make_kernel ("kernel2",
+                            func_kernel2,
+                            table,
+                            from_kernel2,
+                            to_kernel2,
+                            from_detector2
+                            );
+
+        SADF::make_source ("source1", [] (int& out1, const int& inp1) {out1 = inp1 + 1;}, 1, 0, to_kernel1);
+        SADF::make_source ("source2", [] (int& out1, const int& inp1) {out1 = inp1 + 1;}, 1, 0, to_kernel2);
+        SADF::make_sink ("sink1", [] (const int& out) {std::cout <<"kernel1 = " <<out << std::endl;}, from_kernel1);
+        SADF::make_sink ("sink2", [] (const int& out) {std::cout <<"kernel2 = " <<out << std::endl;}, from_kernel2);
+
+
+        // auto constant1 = new SADF::constant<int>("constant1", 1, 0);
+        // constant1->oport1(form_constant);
 
 
         // auto detector12 = new SADF::detector12 <int, scenarios_state, scenarios_state> ("detector12",
@@ -117,14 +151,7 @@ SC_MODULE(top)
         // detector12-> oport1 (from_detector1);
         // detector12-> oport2 (from_detector2);
 
-        SADF::make_detector12 ("detector12"
-                                ,gamma_func_detector,
-                                next_state_func_detector,
-                                output_decode_func_detector,
-                                S1,
-                                from_detector1,
-                                from_detector2,
-                                form_constant);
+
 
         // auto kernel1 = new SADF::kernel <int, scenarios_state, int> ("kernel1",
         //                                                             func_kernel1,
@@ -134,37 +161,29 @@ SC_MODULE(top)
         // kernel1-> control_port (from_detector1);
         // kernel1-> oport1 (from_kernel1);
 
-        SADF::make_kernel ("kernel1",
-                            func_kernel1,
-                            table,
-                            from_kernel1,
-                            to_kernel1,
-                            from_detector1
-                            );
                             
-
-        auto kernel2 = new SADF::kernel <int, scenarios_state, int> ("kernel2",
-                                                                    func_kernel2,
-                                                                    table
-                                                                  );
-        kernel2-> iport1 (to_kernel2);
-        kernel2-> control_port (from_detector2);
-        kernel2-> oport1 (from_kernel2);
-
-
-        auto source1 = new SADF::source <int> ("source1", [] (int& out1, const int& inp1) {out1 = inp1 + 1;}, 1, 0);
-        source1-> oport1 (to_kernel1);
-
-        auto source2 = new SADF::source <int> ("source2", [] (int& out1, const int& inp1) {out1 = inp1 - 1;}, 1, 0);
-        source2-> oport1 (to_kernel2);
+        // auto kernel2 = new SADF::kernel <int, scenarios_state, int> ("kernel2",
+        //                                                             func_kernel2,
+        //                                                             table
+        //                                                           );
+        // kernel2-> iport1 (to_kernel2);
+        // kernel2-> control_port (from_detector2);
+        // kernel2-> oport1 (from_kernel2);
 
 
-        auto sink2 = new SADF::sink <int> ("sink2",[](const int& out) {std::cout << out << std::endl;});
-        sink2-> iport1 (from_kernel2);
+        // auto source1 = new SADF::source <int> ("source1", [] (int& out1, const int& inp1) {out1 = inp1 + 1;}, 1, 0);
+        // source1-> oport1 (to_kernel1);
+
+        // auto source2 = new SADF::source <int> ("source2", [] (int& out1, const int& inp1) {out1 = inp1 - 1;}, 1, 0);
+        // source2-> oport1 (to_kernel2);
 
 
-        auto sink3 = new SADF::sink <int> ("sink3",[](const int& out) {std::cout << out << std::endl;});
-        sink3-> iport1 (from_kernel1);
+        // auto sink2 = new SADF::sink <int> ("sink2",[](const int& out) {std::cout << out << std::endl;});
+        // sink2-> iport1 (from_kernel2);
+
+
+        // auto sink3 = new SADF::sink <int> ("sink3",[](const int& out) {std::cout << out << std::endl;});
+        // sink3-> iport1 (from_kernel1);
 
 
         // auto sink1 = new SADF::sink <scenarios_state> ("sink1",[](const scenarios_state& out) {std::cout << out << std::endl;});
