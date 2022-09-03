5b3d061a9093186fb109063adc6f2d80b6858f24
mohammad <mohammad.vazirpanah@yahoo.com>
2022-09-03 12:26:32 +0430

    Compile without error

--- examples/selfaware/top.hpp
+++ examples/selfaware/top.hpp
@@ -1,200 +1,296 @@
-/**********************************************************************
-    * main.cpp --   *
-    *                                                                 *
-    * Author:  Mohammad Vazirpanah (mohammad.vazirpanah@yahoo.com)    *
-    *                                                                 *
-    * Purpose: Demonstration of a simple program.                     *
-    *                                                                 *
-    * Usage:                                             *
-    *                                                                 *
-    * License: BSD3                                                   *
-    *******************************************************************/
+
 
 #include <forsyde.hpp>
-
+#include <iostream>
+#include <fstream> 
 
 using namespace sc_core;
 using namespace ForSyDe;
 using namespace std;
 
+enum sim_scenario_type {S1 ,S2};
+enum system_scenario_type {sys_resume, sys_stop};
+enum inputabst_scenario_type {inputabst_resume, inputabst_stop};
+enum self_model_scenario_type {self_model_resume, self_model_stop};
+enum outputabst_scenario_type {outputabst_resume, outputabst_stop};
+enum env_scenario_type {evn_resume, evn_stop};
 
 
-enum detector_scenario_type {S1 ,S2, S3, S4};
-enum kernel1_scenario_type {ADD, MINUS};
-enum kernel2_scenario_type {MUL, DIV};
 
-typedef map<
-            detector_scenario_type,
-            array<size_t,2>
-        > detector_scenario_table_type;
+// !! ---------------------- Simulator ----------------------  !! // 
+typedef map<sim_scenario_type, array<size_t,5>> sim_scenario_table_type;
 
-typedef map<
-            kernel1_scenario_type,
-            tuple<array<size_t,1>,array<size_t,1>>
-        > kernel1_scenario_table_type;
-typedef map<
-            kernel2_scenario_type,
-            tuple<array<size_t,1>,array<size_t,1>>
-        > kernel2_scenario_table_type;
 
-detector_scenario_table_type detector1_table = {
-    {S1, {1,1}},
-    {S2, {1,1}},
-    {S3, {1,1}},
-    {S4, {1,1}}
+sim_scenario_table_type sim_table = {
+    {S1, {1,1,1,1,1}},
+    {S2, {1,1,1,1,1}}
 };
 
-kernel1_scenario_table_type kernel1_table = 
+// !! ----------------------  System ----------------------  !! // 
+typedef map<system_scenario_type, tuple<array<size_t,1>,array<size_t,2>>> system_scenario_table_type;
+
+system_scenario_table_type system_table = 
 {  
-    {ADD,   make_tuple(array<size_t,1>{3},array<size_t,1>{1})},
-    {MINUS, make_tuple(array<size_t,1>{2},array<size_t,1>{1})}
+    {sys_resume, make_tuple(array<size_t,1>{1},array<size_t,2>{1,1})},
+    {sys_stop, make_tuple(array<size_t,1>{0},array<size_t,2>{0,0})}
 };
 
-kernel2_scenario_table_type kernel2_table =
-{
-    {MUL, make_tuple(array<size_t,1>{2},array<size_t,1>{1})},
-    {DIV, make_tuple(array<size_t,1>{2},array<size_t,1>{1})}
+// !! ----------------------  Input Abstraction ----------------------  !! // 
+typedef std::map<inputabst_scenario_type, std::tuple<size_t,size_t>> inputabst_scenario_table_type;
+
+inputabst_scenario_table_type inputabst_table = 
+{  
+    {inputabst_resume, std::make_tuple(1,1)},
+    {inputabst_stop, std::make_tuple(0,0)}
+};
+
+// !! ----------------------  Output Abstraction ----------------------  !! // 
+typedef std::map<outputabst_scenario_type, std::tuple<size_t,size_t>> outputabst_scenario_table_type;
+
+outputabst_scenario_table_type outputabst_table = 
+{  
+    {outputabst_resume, std::make_tuple(1,1)},
+    {outputabst_stop, std::make_tuple(0,0)}
+};
+
+// !! ---------------------- Self Model ----------------------  !! // 
+typedef map<self_model_scenario_type, tuple<array<size_t,2>,array<size_t,1>>> self_model_scenario_table_type;
+
+self_model_scenario_table_type self_model_table = 
+{  
+    {self_model_resume, make_tuple(array<size_t,2>{1,1},array<size_t,1>{1})},
+    {self_model_stop, make_tuple(array<size_t,2>{0,0},array<size_t,1>{0})}
+};
+
+// !! ----------------- Abstracted Environment ------------------  !! // 
+typedef std::map<env_scenario_type, std::tuple<size_t,size_t>> env_scenario_table_type;
+
+env_scenario_table_type env_table = 
+{  
+    {evn_resume, std::make_tuple(1,1)},
+    {evn_stop, std::make_tuple(0,0)}
 };
 
 
-void kernel1_func(tuple<vector<int>>& out, 
-                    const kernel1_scenario_type& _scenarios_state, 
-                    const tuple<vector<int>>& inp)
-{
-    auto inp1 = get<0>(inp);
-    auto& out1 = get<0>(out);
-    if (_scenarios_state == ADD)
-        out1[0] = inp1[0] + inp1[1] + inp1[2];
-    else if (_scenarios_state == MINUS)
-        out1[0] = inp1[1]- inp1[0];
+
+void enviroment_behavior (std::vector<int>& out, const std::vector<int>& inp)
+{   
+    out[0] = inp[0]*inp[0]; 
 }
 
-
-void kernel2_func(tuple<vector<int>>& out, 
-                    const kernel2_scenario_type& _scenarios_state, 
-                    const tuple<vector<int>>& inp)
-{
-    auto inp1 = get<0>(inp);
-    auto& out1 = get<0>(out);
-    if (_scenarios_state == MUL)
-        out1[0] = inp1[0] * inp1[1];
-    else if (_scenarios_state == DIV)
-        out1[0] = inp1[0] / inp1[1];
-}
-
-void kernel1_goal (std::vector<bool>& inp)
+void system_goal(std::vector<bool>& goals)
 {
 
 }
 
+void self_model_goal (std::vector<bool>& goals)
+{
 
-void detector1_cds_func(detector_scenario_type& new_scenario, 
-                        const detector_scenario_type& previous_scenario, 
+}
+void system_behavior(std::tuple<std::vector<int>, std::vector<int>>& out,
+                     const system_scenario_type& _system_state,
+                     const std::tuple<std::vector<int>> &inp)
+{
+
+}
+
+void sim_cds_func(sim_scenario_type& new_scenario, 
+                        const sim_scenario_type& previous_scenario, 
                         const tuple<vector<int>>& inp)
 {
-    switch (previous_scenario)
+        switch (previous_scenario)
     {
         case S1:
             new_scenario = S2;
             break;
         case S2:
-            new_scenario = S3;
-            break;
-        case S3:
-            new_scenario = S4;
-            break;
-        case S4:
             new_scenario = S1;
             break;
+
     }
+
 }
 
-
-void detector1_kss_func(tuple<kernel1_scenario_type,kernel2_scenario_type>& out,
-                        const detector_scenario_type& current_scenario,
+void sim_kss_func(tuple<system_scenario_type,
+                        inputabst_scenario_type,
+                        self_model_scenario_type,
+                        outputabst_scenario_type,
+                        env_scenario_type>& out,
+                        const sim_scenario_type& current_scenario,
                         const tuple<vector<int>>& inp)
 {
+
     switch (current_scenario)
     {
         case S1:
-            out = make_tuple(ADD,MUL);
+            out = make_tuple(sys_resume,
+                            inputabst_resume, 
+                            self_model_resume,
+                            outputabst_resume,
+                            evn_resume);
             break;
         case S2:
-            out = make_tuple(MINUS,DIV);
-            break;
-        case S3:
-            out = make_tuple(ADD,MUL);
-            break;
-        case S4:
-            out = make_tuple(MINUS,DIV);
+            out = make_tuple(sys_stop,
+                            inputabst_stop,
+                            self_model_stop,
+                            outputabst_stop,
+                            evn_stop);
             break;
     }
+
+}
+
+void self_model_func(tuple<vector<int>>& out, 
+                    const self_model_scenario_type& _scenarios_state, 
+                    const tuple<vector<int>,vector<int>>& inp)
+{
+
+}
+
+void inp_abst_func(std::vector<int>& out,
+                     const inputabst_scenario_type& _inputabst_state,
+                     const std::vector<int>&inp)
+{
+
 }
 
 
+void out_abst_func(std::vector<int>& out,
+                     const outputabst_scenario_type& _outputabst_state,
+                     const std::vector<int>&inp)
+{
+
+}
+
+void self_env_func(std::vector<int>& out,
+                     const env_scenario_type& _env_state,
+                     const std::vector<int>&inp)
+{
+
+}
+
 SC_MODULE(top)
 {
-    SADF::signal<kernel1_scenario_type> from_detector1; 
-    SADF::signal<kernel2_scenario_type> from_detector2;
-    SADF::signal<int> from_source;
-    SADF::signal<int> to_kernel1, from_kernel1, to_kernel2, from_kernel2;
-    
+
+    SADF::signal<int> to_enviroment, from_enviroment, to_system, from_system,  to_abstract, from_in_abst, from_out_abst, from_self_model, to_self_model, from_env_abst, from_constant;
+    SADF::signal<system_scenario_type> sim_to_system;
+    SADF::signal<inputabst_scenario_type> sim_to_in_abst;
+    SADF::signal<outputabst_scenario_type> sim_to_out_abst;
+    SADF::signal<self_model_scenario_type> sim_to_self_model;
+    SADF::signal<env_scenario_type> sim_to_env_abst;
+
+
+
+
     SC_CTOR(top)
     {
-
-        auto sourced = new SDF::source<int>("sourced", [] (int& out1, const int& inp1) {out1 = inp1 + 1;}, 1, 4);
-        sourced->oport1(from_source);
-
-        auto detector1 = new SADF::simulator<
-                                tuple<kernel1_scenario_type,kernel2_scenario_type>,
-                                tuple<int>,
-                                detector_scenario_type
-                            >
-                            (
-                                "detector1",
-                                detector1_cds_func,
-                                detector1_kss_func,
-                                detector1_table,
-                                S1,
-                                {1}
-                            );
-        get<0>(detector1->iport)(from_source);
-        get<0>(detector1->oport)(from_detector1);
-        get<1>(detector1->oport)(from_detector2);
-
-        auto kernel1 = new SADF::system<tuple<int>,kernel1_scenario_type,tuple<int>>(
-                            "kernel1",
-                            kernel1_func,
-                            kernel1_table,
-                            kernel1_goal
-                        );
-        kernel1->cport1(from_detector1);
-        get<0>(kernel1->iport)(to_kernel1);
-        get<0>(kernel1->oport)(from_kernel1);
-                            
-        auto kernel2 = new SADF::kernelMN<tuple<int>,kernel2_scenario_type,tuple<int>>(
-                            "kernel2",
-                            kernel2_func,
-                            kernel2_table
-                        );
-        kernel2->cport1(from_detector2);
-        get<0>(kernel2->iport)(to_kernel2);
-        get<0>(kernel2->oport)(from_kernel2);
+        SDF::make_constant ("constant", 1, 0, from_constant);
 
 
-        auto source1 = new SDF::source<int>("source1", [] (int& out1, const int& inp1) {out1 = inp1 + 1;}, 1, 0);
-        source1->oport1(to_kernel1);
+        auto environment1 = new SADF::environment<int,int> ("enviroment",enviroment_behavior, 1, 1);
+        environment1->iport1 (to_enviroment);
+        environment1->oport1 (from_enviroment);
+        environment1->oport1 (to_system);
 
-        auto source2 = new SDF::source<int>("source2", [] (int& out1, const int& inp1) {out1 = inp1 - 1;}, -1, 0);
-        source2->oport1(to_kernel2);
+        auto system1 = new SADF::system<
+                                        tuple<int,int>,
+                                        system_scenario_type,
+                                        tuple<int>
+                                        >
+                                        (
+                                        "system1",
+                                        system_behavior,
+                                        system_table,
+                                        system_goal
+                                        );
+        system1->cport1(sim_to_system);
+        get<0>(system1->iport)(to_system);
+        get<0>(system1->oport)(to_enviroment);
+        get<1>(system1->oport)(from_system);
+                                               
+
+        auto inputabst1 = new SADF::kernel<int,inputabst_scenario_type, int>
+                                                    (
+                                                    "inputabst",
+                                                    inp_abst_func,
+                                                    inputabst_table
+                                                    );
+        inputabst1->cport1(sim_to_in_abst);
+        inputabst1->iport1(from_enviroment);
+        inputabst1->oport1(from_in_abst);
+ 
+
+        auto outputabst1 = new SADF::kernel<int,outputabst_scenario_type, int>
+                                                    (
+                                                    "outputabst",
+                                                    out_abst_func,
+                                                    outputabst_table
+                                                    );
+        outputabst1->cport1(sim_to_out_abst);
+        outputabst1->iport1(from_system);
+        outputabst1->oport1(from_out_abst);
+        
+
+        auto selfmodel = new SADF::system<
+                                        tuple<int>,
+                                        self_model_scenario_type,
+                                        tuple<int,int>
+                                        >
+                                        (
+                                        "selfmodel1",
+                                        self_model_func,
+                                        self_model_table,
+                                        self_model_goal
+                                        );
+        selfmodel->cport1(sim_to_self_model);
+        get<0>(selfmodel->iport)(from_in_abst);
+        get<1>(selfmodel->iport)(from_env_abst);
+        get<0>(selfmodel->oport)(from_self_model);
 
 
-        auto sink1 = new SDF::sink<int>("sink1",[](const int& out) {cout <<"kernel1 = " <<out << endl;});
-        sink1->iport1(from_kernel1);
+        SADF::make_kernel("self_env1",
+                    self_env_func,
+                    env_table,
+                    from_env_abst,
+                    sim_to_env_abst,
+                    from_self_model
+                );
+        
+        auto sim = new SADF::simulator<
+                                        tuple<system_scenario_type,
+                                            inputabst_scenario_type,
+                                            self_model_scenario_type,
+                                            outputabst_scenario_type,
+                                            env_scenario_type>,
+                                        tuple<int>,
+                                        sim_scenario_type
+                                        >
+                                    (
+                                        "simulator",
+                                        sim_cds_func,
+                                        sim_kss_func,
+                                        sim_table,
+                                        S1,
+                                        {1}
+                                    );
+                        
+        get<0>(sim->iport)(from_constant);
+        get<0>(sim->oport)(sim_to_system);
+        get<1>(sim->oport)(sim_to_in_abst);
+        get<2>(sim->oport)(sim_to_self_model);
+        get<3>(sim->oport)(sim_to_out_abst);
+        get<4>(sim->oport)(sim_to_env_abst);
+        
+    }   
 
 
-        auto sink2 = new SDF::sink<int>("sink2",[](const int& out) {cout <<"kernel2 = " <<out << endl;;});
-        sink2-> iport1(from_kernel2);
+#ifdef FORSYDE_INTROSPECTION
+    void start_of_simulation()
+    {
+        ForSyDe::XMLExport dumper("gen/");
+        dumper.traverse(this);
     }
+#endif
 
 };
+
